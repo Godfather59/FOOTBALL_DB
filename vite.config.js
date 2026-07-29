2fc586a0a3ea86c8807d5731bf3c7d821a64453c
Godfather <said59987@gmail.com>
2026-07-29 21:43:07 +0100

    Use only zero-key development proxies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-function proxyTarget(baseUrl, prefix, headers = {}) {
+function proxyTarget(baseUrl, prefix) {
   const base = new URL(baseUrl)
   const basePath = base.pathname.replace(/\/$/, '')
   return {
@@ -11,9 +11,6 @@ function proxyTarget(baseUrl, prefix, headers = {}) {
     configure(proxy) {
       proxy.on('proxyReq', proxyReq => {
         proxyReq.setHeader('Accept', 'application/json')
-        for (const [name, value] of Object.entries(headers)) {
-          if (value) proxyReq.setHeader(name, value)
-        }
       })
     }
   }
@@ -27,8 +24,6 @@ export default defineConfig(({ mode }) => {
       proxy: {
         '/api/tm': proxyTarget(env.TMAPI_BASE_URL || 'https://tmapi-alpha.transfermarkt.technology', '/api/tm'),
         '/api/ce': proxyTarget(env.TRANSFERMARKT_CEAPI_BASE_URL || 'https://www.transfermarkt.com.tr/ceapi', '/api/ce'),
-        '/api/af': proxyTarget(env.API_FOOTBALL_BASE_URL || 'https://v3.football.api-sports.io', '/api/af', { 'x-apisports-key': env.API_FOOTBALL_KEY }),
-        '/api/fd': proxyTarget(env.FOOTBALL_DATA_BASE_URL || 'https://api.football-data.org/v4', '/api/fd', { 'X-Auth-Token': env.FOOTBALL_DATA_TOKEN }),
         '/api/tsdb': proxyTarget(env.THESPORTSDB_BASE_URL || 'https://www.thesportsdb.com/api/v1/json/123', '/api/tsdb'),
         '/api/oldb': proxyTarget(env.OPENLIGADB_BASE_URL || 'https://api.openligadb.de', '/api/oldb'),
         '/api/sb': proxyTarget(env.STATSBOMB_OPEN_DATA_BASE_URL || 'https://raw.githubusercontent.com/hudl/open-data/master/data', '/api/sb')
