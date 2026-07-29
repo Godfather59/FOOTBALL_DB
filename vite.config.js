feee835ecf2fd7a2e14049bbd49a1eb4fbd86752
Godfather <said59987@gmail.com>
2026-07-29 21:22:27 +0100

    Add development proxies for free providers

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,37 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+function proxyTarget(baseUrl, prefix, headers = {}) {
+  const base = new URL(baseUrl)
+  const basePath = base.pathname.replace(/\/$/, '')
+  return {
+    target: base.origin,
+    changeOrigin: true,
+    rewrite: path => `${basePath}${path.replace(new RegExp(`^${prefix}`), '')}`,
+    configure(proxy) {
+      proxy.on('proxyReq', proxyReq => {
+        proxyReq.setHeader('Accept', 'application/json')
+        for (const [name, value] of Object.entries(headers)) {
+          if (value) proxyReq.setHeader(name, value)
+        }
+      })
+    }
+  }
+}
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const tmBase = new URL(env.TMAPI_BASE_URL || 'https://tmapi-alpha.transfermarkt.technology')
-  const ceBase = new URL(env.TRANSFERMARKT_CEAPI_BASE_URL || 'https://www.transfermarkt.com.tr/ceapi')
-  const afBase = new URL(env.API_FOOTBALL_BASE_URL || 'https://v3.football.api-sports.io')
-  const cePath = ceBase.pathname.replace(/\/$/, '')
-  const afPath = afBase.pathname.replace(/\/$/, '')
-
   return {
     plugins: [react()],
     server: {
       proxy: {
-        '/api/tm': {
-          target: tmBase.origin,
-          changeOrigin: true,
-          rewrite: path => `${tmBase.pathname.replace(/\/$/, '')}${path.replace(/^\/api\/tm/, '')}`
-        },
-        '/api/ce': {
-          target: ceBase.origin,
-          changeOrigin: true,
-          rewrite: path => `${cePath}${path.replace(/^\/api\/ce/, '')}`
-        },
-        '/api/af': {
-          target: afBase.origin,
-          changeOrigin: true,
-          rewrite: path => `${afPath}${path.replace(/^\/api\/af/, '')}`,
-          configure(proxy) {
-            proxy.on('proxyReq', proxyReq => {
-              proxyReq.setHeader('Accept', 'application/json')
-              if (env.API_FOOTBALL_KEY) proxyReq.setHeader('x-apisports-key', env.API_FOOTBALL_KEY)
-            })
-          }
-        }
+        '/api/tm': proxyTarget(env.TMAPI_BASE_URL || 'https://tmapi-alpha.transfermarkt.technology', '/api/tm'),
+        '/api/ce': proxyTarget(env.TRANSFERMARKT_CEAPI_BASE_URL || 'https://www.transfermarkt.com.tr/ceapi', '/api/ce'),
+        '/api/af': proxyTarget(env.API_FOOTBALL_BASE_URL || 'https://v3.football.api-sports.io', '/api/af', { 'x-apisports-key': env.API_FOOTBALL_KEY }),
+        '/api/fd': proxyTarget(env.FOOTBALL_DATA_BASE_URL || 'https://api.football-data.org/v4', '/api/fd', { 'X-Auth-Token': env.FOOTBALL_DATA_TOKEN }),
+        '/api/tsdb': proxyTarget(env.THESPORTSDB_BASE_URL || 'https://www.thesportsdb.com/api/v1/json/123', '/api/tsdb'),
+        '/api/oldb': proxyTarget(env.OPENLIGADB_BASE_URL || 'https://api.openligadb.de', '/api/oldb'),
+        '/api/sb': proxyTarget(env.STATSBOMB_OPEN_DATA_BASE_URL || 'https://raw.githubusercontent.com/hudl/open-data/master/data', '/api/sb')
       }
     }
   }
